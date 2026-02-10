ec5ca1187b790353c26a4a5579f39eb6e019dc83
Gustavo Porcel <gporcelarias@frba.utn.edu.ar>
2026-02-10 08:28:06 -0300

    problemas con el csrf

--- src/main/java/com/gus/demo/spring/boot/demo/security/SecurityConfig.java
+++ src/main/java/com/gus/demo/spring/boot/demo/security/SecurityConfig.java
@@ -23,6 +23,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(org.springframework.security.config.Customizer.withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(org.springframework.security.config.http.SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/productos/**").hasAnyRole("USER", "ADMIN")
